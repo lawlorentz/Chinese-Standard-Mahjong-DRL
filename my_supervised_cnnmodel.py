297d4f669ce9d029175a47559956c26b3196db0b
lawlorentz <lawlorentz@gmail.com>
2022-11-25 10:54:48 +0800

    监督学习代码完成

--- my_supervised_cnnmodel.py
+++ my_supervised_cnnmodel.py
@@ -2,78 +2,97 @@ from dataset import MahjongGBDataset
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-import new_model_try
+from model import CNNModel
 import torch.nn.functional as F
 import torch
 import time
 import os
 from torch.utils.tensorboard import SummaryWriter
- 
+import argparse
+
+
+def parse_args():
+    parse = argparse.ArgumentParser()  # 2、创建参数对象
+    parse.add_argument('--n_gpu', type=int, default=1)  # 3、往参数对象添加参数
+    parse.add_argument('--logdir', type=str, default='/code/log/')
+    parse.add_argument('--checkpoint', type=str)
+    args = parse.parse_args()  # 4、解析参数对象获得解析对象
+    return args
+
+
 log_writer = SummaryWriter()
-n_gpu=8
+
 
 if __name__ == '__main__':
+    args = parse_args()
     # 集群上跑用这个 logdir = '/code/log/'
-    logdir = '/code/log/' 
+    logdir = args.logdir
+    n_gpu = args.n_gpu
     # resnet_depth=34
-    timestamp=int(time.time())
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+    timestamp = int(time.time())
     os.mkdir(logdir + f'checkpoint_CNNModel_{timestamp}')
-    
+
     # Load dataset
     splitRatio = 0.9
     batchSize = 1024*n_gpu
     trainDataset = MahjongGBDataset(0, 0.9, True)
-    loader = DataLoader(dataset = trainDataset, batch_size = batchSize, shuffle = True)
+    loader = DataLoader(dataset=trainDataset,
+                        batch_size=batchSize, shuffle=True)
     validateDataset = MahjongGBDataset(0.9, 1, False)
-    vloader = DataLoader(dataset = validateDataset, batch_size = batchSize, shuffle = False)
-    
+    vloader = DataLoader(dataset=validateDataset,
+                         batch_size=batchSize, shuffle=False)
+
     # debug
     # for i in range(len(validateDataset)):
     #     a=validateDataset[i]
     #     print(i)
-        
 
     # Load model
-    data_dir='/code/log/checkpoint_CNNModel_1656844401/0.pkl'
-    model = new_model_try.CNNModel().to('cuda')
-    model.load_state_dict(torch.load(data_dir))
-    
+    model = CNNModel().to('cuda')
+    if args.checkpoint is not None:
+        data_dir = args.checkpoint
+        model.load_state_dict(torch.load(data_dir))
+
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(model)
-    
-    
-    optimizer = torch.optim.Adam(model.parameters(), lr = 1e-3)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     scheduler = ReduceLROnPlateau(optimizer, 'min')
     # Train and validate
     for e in range(50):
         print('Epoch', e)
-        
+
         for i, d in enumerate(loader):
-            input_dict = {'is_training': True, 'obs': {'observation': d[0].cuda(), 'action_mask': d[1].cuda()}}
-            logits = model(input_dict)
+            input_dict = {'is_training': True, 'obs': {
+                'observation': d[0].cuda(), 'action_mask': d[1].cuda()}}
+            logits = model(input_dict)[0]
             loss = F.cross_entropy(logits, d[2].long().cuda())
             if i % 128 == 0:
-                print('Iteration %d/%d'%(i, len(trainDataset) // batchSize + 1), 'policy_loss', loss.item())
+                print('Iteration %d/%d' % (i, len(trainDataset) //
+                      batchSize + 1), 'policy_loss', loss.item())
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
-            if i%1024==0:
+
+            if i % 1024 == 0:
                 print('Run validation:')
                 correct = 0
                 for i, d in enumerate(vloader):
-                    input_dict = {'is_training': False, 'obs': {'observation': d[0].cuda(), 'action_mask': d[1].cuda()}}
+                    input_dict = {'is_training': False, 'obs': {
+                        'observation': d[0].cuda(), 'action_mask': d[1].cuda()}}
                     with torch.no_grad():
-                        logits = model(input_dict)
-                        pred = logits.argmax(dim = 1)
+                        logits = model(input_dict)[0]
+                        pred = logits.argmax(dim=1)
                         correct += torch.eq(pred, d[2].cuda()).sum().item()
                 acc = correct / len(validateDataset)
                 print('Epoch', e + 1, 'Validate acc:', acc)
                 scheduler.step(acc)
-        torch.save(model.module.state_dict(), logdir + f'checkpoint_CNNModel_{timestamp}/{e}.pkl')
+        torch.save(model.module.state_dict(), logdir +
+                   f'checkpoint_CNNModel_{timestamp}/{e}.pkl')
         print(logdir + f'checkpoint_CNNModel_{timestamp}/{e}.pkl saved')
-        
-        
+
         # correct = 0
         # for i, d in enumerate(vloader):
         #     input_dict = {'is_training': False, 'obs': {'observation': d[0].cuda(), 'action_mask': d[1].cuda()}}
@@ -85,4 +104,3 @@ if __name__ == '__main__':
         # print('Epoch', e + 1, 'Validate acc:', acc)
         # scheduler.step(acc)
         # log_writer.add_scalar('Accuracy/valid', float(acc), e)
-        
---
+++ run.sh
@@ -0,0 +1,1 @@
+python3 -u my_supervised_cnnmodel.py --n_gpu 2 > /code/train_log/train.log 2>&1 &
